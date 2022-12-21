3af45861d197268b44402685f5918f6cc6bd3d33
nrsia <93438840+nrsia@users.noreply.github.com>
2022-12-21 21:54:46 +0530

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+
+using namespace std;
+void show(int,int);
+void show(double,double);
+void show(char,char);
+int main()
+{
+	show(2,5);
+	show(2.5,5.5);
+	return 0;
+}
+void show(int a,int b)
+{
+	cout<<"a ="<<a<<endl;
+	cout<<"b ="<<b<<endl;
+}
+void show(double a,double b)
+{
+	cout<<"a ="<<a<<endl;
+	cout<<"b ="<<b<<endl;
+}
